43f8343f05e10c0d13293662f3785a3b1eb6dcef
cheskyposen <shragaposen@gmail.com>
2019-03-18 11:10:18 -0400

    routint added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,17 +9,12 @@ import {Hero} from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
   getHeroes(): void {
     this.heroService.getHeroes()
