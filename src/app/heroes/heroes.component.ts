a8d1c8ebfa67356119f37b2950003e157781261c
cheskyposen <shragaposen@gmail.com>
2019-03-17 20:49:08 -0400

    Initial commit 'tour of heroes'

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import {Hero} from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  ngOnInit() {
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
